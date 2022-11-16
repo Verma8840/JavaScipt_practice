a2358bcafe89f83674d672717b8a12f765928a81
virendra8840 <vv0520273@gmail.com>
2022-11-16 19:40:12 +0530

    javascript

---
+++ Oops/JS Class/inharitance.js
@@ -0,0 +1,12 @@
+function Person(first, last, age, eyecolor){
+	this.firstName = first;
+	this.lastName = last;
+	this.age = age;
+	this.eyeColor = eyecolor;
+}
+
+Person.prototype.nationality = "English";
+
+Person.prototype.name = function() {
+	return this.firstName + " " .lastName;
+};
