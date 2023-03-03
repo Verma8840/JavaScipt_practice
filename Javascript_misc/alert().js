3142d4814cf3d600cccf3a03382185f9ae25e65c
virendra8840 <vv0520273@gmail.com>
2023-03-03 17:41:09 +0530

    alert

---
+++ Javascript_misc/alert().js
@@ -0,0 +1,21 @@
+//The alert() method in JavaScript is used to display a virtual alert box. It is mostly used to give a warning message to the users. It displays an alert dialog box that consists of some specified message (which is optional) and an OK button. When the dialog box pops up, we have to click "OK" to proceed
+
+
+<html>  
+   <head>     
+      <script type = "text/javascript">  
+            function fun()  
+  
+  
+               alert ("This is an alert dialog box");  
+              
+      </script>       
+   </head>  
+     
+   <body>  
+      <p> Click the following button to see the effect </p>        
+      <form>  
+         <input type = "button" value = "Click me" onclick = "fun();" />  
+      </form>       
+   </body>  
+</html>  
