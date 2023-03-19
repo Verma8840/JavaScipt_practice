11f56be13b58045021c563cc11dedde7527fcb6e
virendra8840 <vv0520273@gmail.com>
2023-03-19 12:11:02 +0530

    reload method

---
+++ Operater/reload_method.js
@@ -0,0 +1,15 @@
+<script>  
+function fun() {  
+location.reload();  
+}  
+</script>  
+</head>  
+  
+<body>  
+<h1> Welcome to the javaTpoint.com </h1>  
+  
+<h2> This is an example of location.reload() method </h2>  
+  
+<p> Click the following 'Reload' button to see the effect. </p>  
+  
+<button onclick = "fun()"> Reload </button>  
